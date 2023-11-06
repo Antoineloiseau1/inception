87593e8a8c6e82a2d9e35c62b5f27c381cabe1f2
Antoineloiseau1 <antoineloiseau1@gmail.com>
2023-11-06 17:56:10 +0100

    working on db

--- Makefile
+++ Makefile
@@ -7,4 +7,8 @@ CMD		=	up
 all:
 	${DOCKER} ${SRCS} ${CMD} -d
 
+clean:
+	docker container prune -f
+	docker image prune -af
+
 .PHONY:		all clean re
---
+++ srcs/requirements/mariadb/tool/create_db.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+service mariadb start
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mariadb -e "CREATE USER IF NOT EXISTS ${DB_USR}@locahost IDENTIFIED BY '${DB_PWD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USR}@localhost IDENTIFIED BY '${DB_PWD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$DB_ROOT_PWD shutdown
+exec mysqld_safe
