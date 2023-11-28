845ebb5c2db6a7c1854effc4eeae129eab1508dc
antoine <antoineloiseau1@gmail.com>
2023-11-28 17:50:11 +0100

    still on mariadb

--- Makefile
+++ Makefile
@@ -2,19 +2,17 @@ SRCS	=	./srcs/docker-compose.yml
 
 all:
 	# adding login.42.fr to localhost
-	echo "127.0.0.1 anloisea.42.fr" >> /etc/hosts
+	# sudo echo "127.0.0.1 anloisea.42.fr" >> /etc/hosts
 	docker compose -f ${SRCS} up -d --build
 
 down:
 		docker compose -f ${SRCS} down
 
 clean:
-		docker stop $$(docker ps -aq); \
-		docker container rm $$(docker ps -aq); \
-		docker rmi -f $$(docker images -aq); \
+		docker -f srcs/docker-compose.yml down \
 		docker volume rm $$(docker volume ls -q); \
 		docker network rm $$(docker network ls -q); \
 
-re:		clean all
+re:		down all
 
-.PHONY:	all down re
+.PHONY:	all down clean re
--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
-service mysql start
+if [ ! -d "/var/run/mysqld" ]; then
+	mkdir -p /var/run/mysqld
+fi
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-mariadb -e "CREATE USER IF NOT EXISTS '${DB_USR}'@'%' IDENTIFIED BY '${DB_PWD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON '${DB_NAME}'.* TO '${DB_USR}'@'%' IDENTIFIED BY '${DB_PWD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
+chown mysql:mysql /var/run/mysqld
+chmod 777 /var/run/mysqld
+
+service mariadb start
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+echo	"database Created"
+mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+echo	"User ${MYSQL_USR} Created"
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USR}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -e "FLUSH PRIVILEGES;"
+echo "all privileges granted"
+mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+echo "root altered"
 
-mysqladmin -u root -p'$DB_ROOT_PWD' shutdown
+#mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
-exec mysqld_safe
+#exec mysqld_safe
