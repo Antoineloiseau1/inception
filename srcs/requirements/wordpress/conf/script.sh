7b3904781171e2a07709f5e2118f01995d08f1f9
Antoineloiseau1 <antoineloiseau1@gmail.com>
2023-12-08 14:15:53 +0100

    wordpress won't connect to mariadb

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -15,13 +15,14 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 
 rm /var/www/html/wp-config-sample.php 
-mv /wp-config.php /var/www/html/wp-config.php
 
 # Replace MariaDB info with env variables in wp-config
-sed -i 's/database_name_here/'$MYSQL_DATABASE'/1' /var/www/html/wp-config.php
-sed -i 's/username_here/'$MYSQL_USR'/1' /var/www/html/wp-config.php
-sed -i 's/password_here/'$MYSQL_PASSWORD'/1' /var/www/html/wp-config.php
-sed -i 's/localhost/mariadb:3306/1' /var/www/html/wp-config.php
+sed -i 's/database_name_here/'$MYSQL_DATABASE'/1' /wp-config.php
+sed -i 's/username_here/'$MYSQL_USR'/1' /wp-config.php
+sed -i 's/password_here/'$MYSQL_PASSWORD'/1' /wp-config.php
+sed -i 's/localhost/mariadb:3306/1' /wp-config.php
+
+mv /wp-config.php /var/www/html/wp-config.php
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
@@ -33,7 +34,7 @@ wp plugin install redis-cache --activate --allow-root
 
 wp plugin update --all --allow-root
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 sed -i 's/;clear_env = no/clear_env = no/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir -p /run/php
